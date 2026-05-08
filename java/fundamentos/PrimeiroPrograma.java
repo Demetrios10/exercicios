2ea3766287088dadc397619d43ec096b06010fc4
Demetrios Alves Da Silva <70618050+Demetrios10@users.noreply.github.com>
2026-05-08 16:10:21 -0300

    Add files via upload

---
+++ java/fundamentos/PrimeiroPrograma.java
@@ -0,0 +1,8 @@
+package fundamentos;
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        System.out.println("Olá, Mundo!");
+        System.out.println("Bem-vindo ao Mundo Java!");
+        System.out.println("Vamos aprender a programar juntos!");
+    }
+}
