2be3dc2b5018909a655aee9089922f0bc4323119
Demetrios Alves <demetrios.10@hotmail.com>
2026-05-08 16:16:55 -0300

    update

--- java/fundamentos/PrimeiroPrograma.java
+++
@@ -1,8 +0,0 @@
-package fundamentos;
-public class PrimeiroPrograma {
-    public static void main(String[] args) {
-        System.out.println("Olá, Mundo!");
-        System.out.println("Bem-vindo ao Mundo Java!");
-        System.out.println("Vamos aprender a programar juntos!");
-    }
-}
