f9d8474319057a0cb835143d365a19a095916949
Demetrios Alves <demetrios.10@hotmail.com>
2026-05-09 19:26:00 -0300

    update

--- fundamentos/comentarios.java
+++ fundamentos/comentarios.java
@@ -1,6 +1,6 @@
 package fundamentos;
 
-public class comentarios {
+public class Comentarios {
 public static void main(String[] args) {
 
     System.out.println("tudo o que esta abaixo é comentario em java!");
