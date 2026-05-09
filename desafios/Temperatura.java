59f9ece15bd3ec526ac6d91ba3d79468c938512c
Demetrios Alves <demetrios.10@hotmail.com>
2026-05-09 19:38:26 -0300

    update

---
+++ desafios/Temperatura.java
@@ -0,0 +1,10 @@
+package desafios;
+
+public class Temperatura {
+    public static void main(String[] args) {
+        double celsius = 30.0;
+        double fahrenheit = (celsius * 9/5) + 32;
+        System.out.println(celsius + " graus Celsius é igual a " + fahrenheit + " graus Fahrenheit.");
+    }
+    
+}
