710ae181c6e7544d40d953d12db4a69f88e64f64
Demetrios Alves <demetrios.10@hotmail.com>
2026-05-09 19:27:34 -0300

    up

---
+++ fundamentos/AreaDaCircunferencia.java
@@ -0,0 +1,10 @@
+package fundamentos;
+
+public class AreaDaCircunferencia {
+    public static void main(String[] args) {
+        double raio = 3.4;
+        double area = Math.PI * Math.pow(raio, 2);
+        System.out.println("A área da circunferência é: " + area);
+    }
+    
+}
