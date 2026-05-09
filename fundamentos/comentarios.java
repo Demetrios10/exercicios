656585c6b47fd8a43a0d152db5c783644c3c887b
Demetrios Alves <demetrios.10@hotmail.com>
2026-05-09 19:20:01 -0300

    update

---
+++ fundamentos/comentarios.java
@@ -0,0 +1,23 @@
+package fundamentos;
+
+public class comentarios {
+public static void main(String[] args) {
+
+    System.out.println("Olá, Mundo!");
+    }
+
+}
+
+// Comentário de linha única
+
+/*
+ * Comentário de múltiplas linhas
+ * Pode ser usado para explicar blocos de código ou fornecer informações adicionais.
+ */
+
+
+/*
+aqui tb consigo fazer um comentário de múltiplas linhas
+
+pois o java ignora tudo que estiver entre /* e * / (sem o espaço)
+*/
