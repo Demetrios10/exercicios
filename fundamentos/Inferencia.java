86cde6901fa2a1bdc70133b4b6ac6fe29e52fdc7
Demetrios Alves <demetrios.10@hotmail.com>
2026-05-09 19:51:04 -0300

    update

---
+++ fundamentos/Inferencia.java
@@ -0,0 +1,15 @@
+package fundamentos;
+
+
+public class Inferencia {
+    public static void main(String[] args) {
+        var nome = "João"; // O tipo é inferido como String
+        var idade = 30; // O tipo é inferido como int
+        var salario = 4500.50; // O tipo é inferido como double
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Salário: " + salario);
+    }
+    
+}
