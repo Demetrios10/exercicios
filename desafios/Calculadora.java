b779a7d06bfa188ecbd958d2e14345033c12b3aa
Demetrios Alves <demetrios.10@hotmail.com>
2026-05-09 19:41:01 -0300

    update

---
+++ desafios/Calculadora.java
@@ -0,0 +1,19 @@
+package desafios;
+
+public class Calculadora {
+    public static void main(String[] args) {
+        double num1 = 10.0;
+        double num2 = 5.0;
+
+        double soma = num1 + num2;
+        double subtracao = num1 - num2;
+        double multiplicacao = num1 * num2;
+        double divisao = num1 / num2;
+
+        System.out.println("Soma: " + soma);
+        System.out.println("Subtração: " + subtracao);
+        System.out.println("Multiplicação: " + multiplicacao);
+        System.out.println("Divisão: " + divisao);
+    }
+    
+}
