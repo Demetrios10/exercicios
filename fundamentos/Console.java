77d0d33833663f207a0c41500745ea7e94e78471
Demetrios Alves <demetrios.10@hotmail.com>
2026-05-11 19:33:09 -0300

    update

--- fundamentos/Console.java
+++ fundamentos/Console.java
@@ -1,5 +1,7 @@
 package fundamentos;
 
+import java.util.Scanner;
+
 public class Console {
     
     public static void main(String[] args) {
@@ -12,6 +14,17 @@ public class Console {
 
         System.out.printf("Megasena: %d %d %d %d %d %d\n", 1, 2, 3, 4, 5, 6);
         System.out.printf("Salário: R$ %.2f\n", 1234.5678);
+
+
+
+        System.out.print("Digite o seu nome: ");
+        Scanner entrada = new Scanner(System.in); // System.in é o fluxo de entrada do teclado
+
+        String nome = entrada.nextLine(); // nextLine() lê a linha inteira digitada pelo usuário, incluindo espaços em branco
+        
+        System.out.println("Olá, " + nome + "!");
+        
+        entrada.close(); // Fechar o scanner para liberar recursos do sistema
 
     }
     
