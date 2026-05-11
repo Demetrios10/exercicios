96726d3b340110a4acde72c06b629831fe520d5c
Demetrios Alves <demetrios.10@hotmail.com>
2026-05-11 18:59:18 -0300

    Update

--- fundamentos/TipoString.java
+++ fundamentos/TipoString.java
@@ -6,9 +6,15 @@ public class TipoString {
 
         String s = "Boa Noite";
 
-        System.out.println(s.charAt(2));
+        System.out.println(s.charAt(2)); // Imprime o caractere na posição 2 (a letra 'a')
+        System.out.println(s.length()); // Imprime o comprimento da string (9)
+        System.out.println(s.concat("!!!")); // Imprime a string concatenada com "!!!"
+        System.out.println(s.startsWith("Boa")); // Verifica se a string começa com "Boa" (true)
+        System.out.println(s.toUpperCase()); // Imprime a string em maiúsculas
+        System.out.println(s.toLowerCase()); // Imprime a string em minúsculas
 
-        System.out.println(s.concat("!!!"));
+
+        
 
     }
 }
